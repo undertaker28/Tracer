1c3f23b7a9a5e7e663d53bf8b7c7e94b37a768fb
Pavel Starovoytov <pavel.starovoytovvv@gmail.com>
2022-09-26 23:47:10 +0300

    Add Example with loading plugins

---
+++ Tracer/Example/Program.cs
@@ -0,0 +1,37 @@
+﻿using Core.Tracers;
+using Tests;
+using Tracer.Serialization;
+
+var tracer = new MainTracer();
+var test = new TestMethods(tracer);
+
+var t1 = new Thread(() =>
+{
+	test.M1();
+	//test.M2();
+	//test.M3();
+});
+t1.Start();
+
+var t2 = new Thread(() =>
+{
+	//test.M1();
+	test.M2();
+	//test.M3();
+});
+t2.Start();
+
+t1.Join();
+t2.Join();
+
+var traceResult = tracer.GetTraceResult();
+
+var pluginPath = "../../../Plugins/";
+
+var pluginList = PluginLoader.Load(pluginPath);
+
+for (int i = 1; i <= pluginList.Count; i++)
+{
+	using var fileStream = new FileStream(pluginPath + $"test{i}.txt", FileMode.Create, FileAccess.Write);
+	pluginList[i - 1].Serialize(traceResult, fileStream);
+}
