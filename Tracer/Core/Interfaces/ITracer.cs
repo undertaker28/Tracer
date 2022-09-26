8efdfc49ffad9a698156697a43f3d05e409b10bb
Pavel Starovoytov <pavel.starovoytovvv@gmail.com>
2022-09-26 21:56:59 +0300

    Add Interface in Core

---
+++ Tracer/Core/Interfaces/ITracer.cs
@@ -0,0 +1,14 @@
+﻿namespace Core.Interfaces
+{
+	public interface ITracer<T>
+	{
+        // Call at the beginning of the measured method
+        void StartTrace();
+
+        // Call at the end of the measured method
+        void StopTrace();
+
+        // Get measurement result
+        T GetTraceResult();
+    }
+}
