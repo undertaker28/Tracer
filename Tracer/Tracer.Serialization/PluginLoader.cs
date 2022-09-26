2ef44586b714de8a3e3ddf075162a25b5401d7d0
Pavel Starovoytov <pavel.starovoytovvv@gmail.com>
2022-09-27 01:08:30 +0300

    Add Tracer.Serialization

---
+++ Tracer/Tracer.Serialization/PluginLoader.cs
@@ -0,0 +1,35 @@
+﻿using Serialization.Abstractions;
+using System.Reflection;
+
+namespace Tracer.Serialization
+{
+	public static class PluginLoader
+	{
+
+		public static List<ITraceResultSerializer> Load(string directory)
+		{
+			var result = new List<ITraceResultSerializer>();
+
+			DirectoryInfo pluginDirectory = new DirectoryInfo(directory);
+			if (!pluginDirectory.Exists)
+				pluginDirectory.Create();
+
+			var pluginFiles = Directory.GetFiles(directory, "*.dll");
+            var interfaceType = typeof(ITraceResultSerializer);
+            foreach (var file in pluginFiles)
+			{
+                var assembly = Assembly.LoadFrom(file);
+
+                var types = assembly.GetTypes()
+                    .Where(t => t.GetInterfaces().Contains(interfaceType));
+
+                foreach (var type in types)
+                {
+                    var plugin = Activator.CreateInstance(type) as ITraceResultSerializer;
+                    result.Add(plugin);
+                }
+            }
+			return result;
+        }
+	}
+}
