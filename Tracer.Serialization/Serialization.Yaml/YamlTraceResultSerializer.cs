39490a7073ea6f33ab5047d00c8482f73027486a
Pavel Starovoytov <pavel.starovoytovvv@gmail.com>
2022-09-27 12:17:11 +0300

    Add Tracer.Serialization(YAML)

---
+++ Tracer.Serialization/Serialization.Yaml/YamlTraceResultSerializer.cs
@@ -0,0 +1,28 @@
+﻿using Core.Models;
+using Serialization.Abstractions;
+using System.Net.WebSockets;
+using System.Text;
+using YamlDotNet.Serialization;
+using YamlDotNet.Serialization.NamingConventions;
+
+namespace Serialization.Json
+{
+	public class YamlTraceResultSerializer : ITraceResultSerializer
+	{
+		public void Serialize(TraceResult traceResult, Stream to)
+		{
+			var result = Mapper.MapToSerializationModel(traceResult);
+			
+			var serializer = new SerializerBuilder()
+				.WithNamingConvention(CamelCaseNamingConvention.Instance)
+				.WithIndentedSequences()
+				.Build();
+
+			var yamlResult = serializer.Serialize(result);
+
+			using var sw = new StreamWriter(to);
+			sw.Write(yamlResult);
+			sw.Flush();	
+		}
+	}
+}
